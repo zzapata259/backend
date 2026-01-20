f8de3b723584450c06617ebcfe8236626402c894
Franco <zzapata259@gmail.com>
2026-01-20 16:12:51 -0300

    mensaje del avance

--- mi-proyecto-express/data/auth.data.js
+++
@@ -1,5 +0,0 @@
-// Guardaremos usuarios en memoria temporalmente
-// Cada usuario: { username, password }
-const usuarios = [];
-
-module.exports = usuarios;
--- mi-proyecto-express/data/partidas.data.js
+++
@@ -1,5 +0,0 @@
-// Lista de partidas activas
-// Cada partida: { id, jugador, materia, preguntas, indicePregunta, respuestas }
-const partidas = [];
-
-module.exports = partidas;
